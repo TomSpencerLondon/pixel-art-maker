b226ef3d291d8a6c6ae28628a572a124625cf605
tomspencerlondon <tomspencer_uk@yahoo.co.uk>
2018-02-21 11:47:41 +0000

    Add pixelCanvas

--- designs.js
+++ designs.js
@@ -2,9 +2,34 @@
 // Select size input
 
 // When size is submitted by the user, call makeGrid()
+$(window).ready(function () {
 
-function makeGrid() {
+function makeGrid(height, width) {
 
-// Your code goes here!
+  let pixelCanvas, grid; 
+  pixelCanvas = $('#pixelCanvas'); 
+
+  if(pixelCanvas.children().length) {
+    pixelCanvas.empty(); 
+  }
+
+  for(let i = 0; i < height; i++){
+    grid += '<tr>'; 
+    for(let j = 0; j < width; j++){
+      grid += '<td></td>'; 
+    }
+      grid += '</td>'
+  }
+  pixelCanvas.html(grid); 
+  pixelCanvas.css("background-color", "#ffffff");
 
 }
+	$('#sizePicker').click(function(evt) {
+    let height, width;
+    width = $('#inputWidth').val();
+    height = $('#inputHeight').val();
+		evt.preventDefault();
+		makeGrid(height, width);
+  });
+  
+});
