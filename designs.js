297d39f712c07b49c21610d3d770b64c5e03692e
tomspencerlondon <tomspencer_uk@yahoo.co.uk>
2018-03-03 16:19:38 +0000

    Run through JavaScript beautifier and also change id for height and width selectors

--- designs.js
+++ designs.js
@@ -1,29 +1,29 @@
-$(window).ready(function () {
-//Create the grid
+$(window).ready(function() {
+  //Create the grid
   function makeGrid(height, width) {
-    
-    for(let i = 0; i < height; i++){
-      $('#pixelCanvas').append('<tr></tr>'); 
-      for(let j = 0; j < width; j++){
-        $('tr:last').append('<td class="cells"></td>');
+
+      for (let i = 0; i < height; i++) {
+          $('#pixelCanvas').append('<tr></tr>');
+          for (let j = 0; j < width; j++) {
+              $('tr:last').append('<td class="cells"></td>');
+          }
+
       }
-      
-    }
-    //Add event clicker for each pixel
-    $('.cells').click(function(event) {
-      let color = $('#colorPicker').val(); 
-      event.preventDefault(); 
-      $(event.target).css('background-color', color); 
-    });
+      //Add event clicker for each pixel
+      $('.cells').click(function(event) {
+          let color = $('#colorPicker').val();
+          event.preventDefault();
+          $(event.target).css('background-color', color);
+      });
   }
-//Event picker for the sizePicker
-$('#sizePicker').submit(function(evt) {
-  $('tr').remove();
-  let height, width;
-  width = $('#inputWidth').val();
-  height = $('#inputHeight').val();
-  evt.preventDefault();
-  makeGrid(height, width);
-});
+  //Event picker for the sizePicker
+  $('#sizePicker').submit(function(evt) {
+      $('tr').remove();
+      let height, width;
+      width = $('#inputWidth').val();
+      height = $('#inputHeight').val();
+      evt.preventDefault();
+      makeGrid(height, width);
+  });
 
 });
